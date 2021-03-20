3c744d96a39b10e2284d3eaee085096eab28bf72
Ahmed Rezk <77725184+atheistcode@users.noreply.github.com>
2021-03-21 01:02:56 +0200

    Added side navigation functionality and modified global and element styles.

---
+++ js/sideNav.js
@@ -0,0 +1,26 @@
+const sideNav = document.querySelector(".side-navigation");
+const menuBtn = document.querySelector(".extras__menu-icon");
+const closeBtn = document.querySelector(".close-btn");
+const subMenuBtns = document.querySelectorAll(".sidenav-sub-menu-btn");
+
+const openSideNav = () => {
+  sideNav.style.display = "block";
+};
+
+const closeSideNav = () => {
+  sideNav.style.display = "none";
+};
+
+const toggleSubMenu = (e) => {
+  const subMenu = e.target.closest(".sidenav-list__item").nextElementSibling;
+  const btn = e.target.closest(".sidenav-sub-menu-btn");
+
+  subMenu.style.display = subMenu.style.display === "block" ? "none" : "block";
+
+  btn.style.transform = btn.style.transform === "rotate(90deg)" ? "rotate(0)" : "rotate(90deg)";
+};
+
+menuBtn.addEventListener("click", openSideNav);
+closeBtn.addEventListener("click", closeSideNav);
+
+subMenuBtns.forEach((btn) => btn.addEventListener("click", (e) => toggleSubMenu(e)));
